6695cba953a1744481f9bea9f79b0fa381d60109
Kevin Mills <kevin.mills226@gmail.com>
2018-10-31 02:52:43 -0400

    Everything.

---
+++ functions.s
@@ -0,0 +1,6 @@
+.definelabel hook, 0x08043182
+.definelabel noevo_return, 0x080431A2
+.definelabel doevo_return, 0x0804317C
+
+.definelabel pokemon_getattr, 0x0803FBE8 
+.definelabel pokemon_species_get_gender_info, 0x0803F78C 
